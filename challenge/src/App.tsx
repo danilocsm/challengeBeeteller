0476ab6961f40c53d26df3146d13ad1a4118a116
Danilo Medeios <danilo.medeiros@splab.ufcg.edu.br>
2023-03-14 16:57:04 -0300

    new: added header (not responsive)

--- challenge/src/App.tsx
+++ challenge/src/App.tsx
@@ -1,5 +1,24 @@
+import React, { useState } from "react";
+import Header from "./components/Header";
+
+export const AppContext = React.createContext({ isLoadingData: false });
+
 function App() {
-  return <h1>Hello world</h1>;
+  const [isLoadingData, setIsLoadingData] = useState<boolean>(true);
+
+  return (
+    <>
+      <AppContext.Provider value={{ isLoadingData }}>
+        <Header />
+      </AppContext.Provider>
+      <button
+        onClick={() => setIsLoadingData(!isLoadingData)}
+        className="rounded-xl w-fit h-fit bg-black text-white m-3 p-4"
+      >
+        Simulate loading state
+      </button>
+    </>
+  );
 }
 
 export default App;
---
+++ challenge/src/components/Header.tsx
@@ -0,0 +1,48 @@
+import { Bell, List } from "@phosphor-icons/react";
+import Link from "./Link";
+import RoundedButton from "./RoundedButton";
+import logo from "../assets/brand_name.png";
+import profilePicture from "../assets/profile_picture.png";
+import { useContext } from "react";
+import { AppContext } from "../App";
+
+const links = [
+  { text: "Home", href: "/" },
+  { text: "Extrato", href: "/" },
+  { text: "Área Pix", href: "/" },
+  { text: "Cartão de crédito", href: "/" },
+];
+
+export default function Header() {
+  const { isLoadingData } = useContext(AppContext);
+
+  return (
+    <nav className="flex flex-row justify-between items-center border-b-2 p-6">
+      <img src={logo} />
+      <div className="flex flex-row gap-x-2">
+        {links.map((link) => (
+          <Link text={link.text} href={link.href} />
+        ))}
+      </div>
+      <div className="flex flex-row gap-x-2">
+        <RoundedButton onClick={() => alert("clicked")}>
+          <Bell
+            size={25}
+            className={`${isLoadingData ? `text-[#DBDBDB]` : `text-black`}`}
+          />
+        </RoundedButton>
+        <RoundedButton onClick={() => console.log("clicked")}>
+          <List
+            size={25}
+            className={`${isLoadingData ? `text-[#DBDBDB]` : `text-black`}`}
+          />
+        </RoundedButton>
+        <RoundedButton onClick={() => console.log("clicked")}>
+          {!isLoadingData && (
+            <img className="w-full h-full" src={profilePicture} />
+          )}
+        </RoundedButton>
+      </div>
+    </nav>
+  );
+}
