c9507ba1964ef9c110e1fb6aaaeeca2f6bc6ffea
Danilo Medeios <danilo.medeiros@splab.ufcg.edu.br>
2023-03-14 20:50:16 -0300

    new: added eslint & prettier configs

--- challenge/src/components/Header.tsx
+++ challenge/src/components/Header.tsx
@@ -1,31 +1,31 @@
-import { Bell, List } from "@phosphor-icons/react";
-import Link from "./Link";
-import RoundedButton from "./RoundedButton";
-import logo from "../assets/brand_name.png";
-import profilePicture from "../assets/profile_picture.png";
-import { useContext } from "react";
-import { AppContext } from "../App";
+import { Bell, List } from '@phosphor-icons/react';
+import Link from './Link';
+import RoundedButton from './RoundedButton';
+import logo from '../assets/brand_name.png';
+import profilePicture from '../assets/profile_picture.png';
+import { useContext } from 'react';
+import { AppContext } from '../App';
 
 const links = [
-  { text: "Home", href: "/" },
-  { text: "Extrato", href: "/" },
-  { text: "Área Pix", href: "/" },
-  { text: "Cartão de crédito", href: "/" },
+  { text: 'Home', href: '/' },
+  { text: 'Extrato', href: '/' },
+  { text: 'Área Pix', href: '/' },
+  { text: 'Cartão de crédito', href: '/' },
 ];
 
 export default function Header() {
   const { isLoadingData } = useContext(AppContext);
 
   return (
-    <nav className="flex flex-row justify-between items-center border-b-2 p-6">
+    <nav className='flex flex-row justify-between items-center border-b-2 p-6'>
       <img src={logo} />
-      <div className="flex flex-row gap-x-2">
+      <div className='flex flex-row gap-x-2'>
         {links.map((link) => (
-          <Link text={link.text} href={link.href} />
+          <Link key={link.text} text={link.text} href={link.href} />
         ))}
       </div>
-      <div className="flex flex-row gap-x-2">
-        <RoundedButton onClick={() => alert("clicked")}>
+      <div className='flex flex-row gap-x-2'>
+        <RoundedButton onClick={() => alert('clicked')}>
           <Bell
             size={25}
             className={`${
@@ -33,7 +33,7 @@ export default function Header() {
             } transition-colors duration-500 ease-in-out`}
           />
         </RoundedButton>
-        <RoundedButton onClick={() => console.log("clicked")}>
+        <RoundedButton onClick={() => console.log('clicked')}>
           <List
             size={25}
             className={`${
@@ -41,9 +41,9 @@ export default function Header() {
             } transition-colors duration-500 ease-in-out`}
           />
         </RoundedButton>
-        <RoundedButton onClick={() => console.log("clicked")}>
+        <RoundedButton onClick={() => console.log('clicked')}>
           {!isLoadingData && (
-            <img className="w-full h-full" src={profilePicture} />
+            <img className='w-full h-full' src={profilePicture} />
           )}
         </RoundedButton>
       </div>
